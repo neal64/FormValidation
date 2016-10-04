3de22c88d393b9d174a763a9e3354cc92ab5ed06
Nilesh <nealpatel64@gmail.com>
2016-10-04 01:04:17 -0700

    Form Validation
    Login form and validate the enter data using AngularJS no server load,
    all process from client side.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("myApp",[]);
+
+app.controller('RegistrationController',['$scope',function($scope){
+
+
+  $scope.register = function() {
+    $scope.message = 'Welcome ' + $scope.user.firstName;
+  };
+
+}]);
+
+
+
